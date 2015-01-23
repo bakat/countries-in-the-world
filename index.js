976bc056837fd1999c203134b068df993b7f379e
Hengkiardo <hengki.sihombing@gmail.com>
2015-01-23 11:14:35 +0700

    getCountryByCode and getCountryByName

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./countries.json');
+var data = require('./countries.json');
+var _find = require('lodash.find')
+
+var countries = {}
+
+
+countries.getCountryByCode = function (countryCode) {
+  var country = _find(countries, function(co) { return co.cca2 === countryCode })
+  return country;
+}
+
+countries.getCountryByName = function (name) {
+  var country = _find(countries, function(co) { return co.name.common === name })
+  return country.name.common;
+}
+
+countries.all = data
+
+module.exports = countries
