e4c95e27a01352533d8ee0553b6924afeab3d164
Hengkiardo <hengki.sihombing@gmail.com>
2015-03-31 22:32:04 +0700

    return null if country not exist

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ var countries = {}
 
 countries.getCountryByCode = function (countryCode) {
   var country = _find(data, function(co) { return co.cca2 === countryCode })
-  return country;
+  if (country) {
+  	return country;
+  } else {
+  	return null
+  }
 }
 
 countries.getCountryByName = function (name) {
   var country = _find(data, function(co) { return co.name.common === name })
-  return country;
+  if (country) {
+  	return country;
+  } else {
+  	return null
+  }
 }
 
 countries.getNameById = function (countryCode) {
   var country = _find(data, function(co) { return co.cca2 === countryCode })
-  return country.name.common;
+  if (country) {
+  	return country.name.common;
+  } else {
+  	return null
+  }
 }
 
 countries.all = data
